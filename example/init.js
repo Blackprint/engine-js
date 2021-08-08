38121eb81b83dbcb7fe401099314f5d35fe3c7d8
StefansArya <stefansarya1@gmail.com>
2021-08-08 21:44:29 +0700

    Update example for current version

--- example/init.js
+++ example/init.js
@@ -67,19 +67,19 @@ let instance = new Engine();
 		// iface.interface = undefined; // Let's use default node interface
 
 		// Handle all output port here
-		node.outputs = {
+		node.output = {
 			Result:Number,
 		};
 
 		// Handle all input port here
-		var inputs = node.inputs = {
+		var input = node.input = {
 			Exec: Blackprint.PortTrigger(function(){
-				node.outputs.Result = multiply();
-				console.log("Result has been set:", node.outputs.Result);
+				node.output.Result = multiply();
+				console.log("Result has been set:", node.output.Result);
 			}),
 			A: Number,
 			B: Blackprint.PortValidator(Number, function(val){
-				// Executed when inputs.B is being obtained
+				// Executed when input.B is being obtained
 				// And the output from other node is being assigned
 				// as current port value in this node
 				console.log(iface.title, '- Port B got input:', val);
@@ -89,14 +89,14 @@ let instance = new Engine();
 
 		// Your own processing mechanism
 		function multiply(){
-			console.log('Multiplying', inputs.A, 'with', inputs.B);
-			return inputs.A * inputs.B;
+			console.log('Multiplying', input.A, 'with', input.B);
+			return input.A * input.B;
 		}
 
 		// When any output value from other node are updated
 		// Let's immediately change current node result
 		node.update = function(cable){
-			node.outputs.Result = multiply();
+			node.output.Result = multiply();
 		}
 
 		// Event listener can only be registered after handle init
@@ -113,15 +113,15 @@ let instance = new Engine();
 
 		// iface.interface = undefined; // Let's use default node interface
 
-		node.outputs = {
+		node.output = {
 			Out:Number
 		};
 
 		var executed = false;
-		node.inputs = {
+		node.input = {
 			'Re-seed':Blackprint.PortTrigger(function(){
 				executed = true;
-				node.outputs.Out = Math.round(Math.random()*100);
+				node.output.Out = Math.round(Math.random()*100);
 			})
 		};
 
@@ -135,7 +135,7 @@ let instance = new Engine();
 			console.warn('Value request for port:', port.name, "from node:", iface2.title);
 
 			// Let's create the value for him
-			node.inputs['Re-seed']();
+			node.input['Re-seed']();
 		}
 	});
 
@@ -146,7 +146,7 @@ let instance = new Engine();
 		// Let's use ../nodes/Logger.js
 		iface.interface = 'i-logger';
 
-		node.inputs = {
+		node.input = {
 			Any: Blackprint.PortArrayOf(null) // Any data type, and can be used for many cable
 		};
 
@@ -167,15 +167,15 @@ let instance = new Engine();
 			// Let's show data after new cable was connected or disconnected
 			iface.on('cable.connect cable.disconnect', function(){
 				console.log("A cable was changed on Logger, now refresing the input element");
-				refreshLogger(node.inputs.Any);
+				refreshLogger(node.input.Any);
 			});
 
-			iface.inputs.Any.on('value', function(port){
+			iface.input.Any.on('value', function(port){
 				console.log("I connected to", port.name, "port from", port.iface.title, "that have new value:", port.value);
 
 				// Let's take all data from all connected nodes
 				// Instead showing new single data-> val
-				refreshLogger(node.inputs.Any);
+				refreshLogger(node.input.Any);
 			});
 		}
 	});
@@ -188,14 +188,14 @@ let instance = new Engine();
 		iface.interface = 'i-button';
 
 		// node = under Blackprint node flow control
-		node.outputs = {
+		node.output = {
 			Clicked:Function
 		};
 
-		// Proxy event object from: node.clicked -> node.clicked -> outputs.Clicked
+		// Proxy event object from: node.clicked -> node.clicked -> output.Clicked
 		node.clicked = function(ev){
 			console.log('button/Simple: got', ev, "time to trigger to the other node");
-			node.outputs.Clicked(ev);
+			node.output.Clicked(ev);
 		}
 	});
 
@@ -207,7 +207,7 @@ let instance = new Engine();
 		iface.interface = 'i-input';
 
 		// node = under Blackprint node flow control
-		node.outputs = {
+		node.output = {
 			Changed:Function,
 			Value:String, // Default to empty string
 		};
@@ -222,11 +222,11 @@ let instance = new Engine();
 			console.warn("Imported data:", JSON.stringify(data));
 
 			iface.data = data;
-			node.outputs.Value = data.value;
+			node.output.Value = data.value;
 		}
 
-		// Proxy string value from: node.changed -> node.changed -> outputs.Value
-		// And also call outputs.Changed() if connected to other node
+		// Proxy string value from: node.changed -> node.changed -> output.Value
+		// And also call output.Changed() if connected to other node
 		node.changed = function(text, ev){
 			// This node still being imported
 			if(iface.importing !== false)
@@ -235,30 +235,31 @@ let instance = new Engine();
 			console.log('The input box have new value:', text);
 
 			// node.data.value === text;
-			node.outputs.Value = iface.data.value;
+			node.output.Value = iface.data.value;
 
 			// This will call every connected node
-			node.outputs.Changed();
+			node.output.Changed();
 		}
 	});
 
 // === Import JSON after all nodes was registered ===
 // You can import this to Blackprint Sketch if you want to view the nodes visually
-instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"outputs":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"outputs":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"outputs":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"x":763,"y":169}],"Example/Button/Simple":[{"i":4,"x":41,"y":59,"outputs":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"x":38,"y":281,"data":{"value":"saved input"},"outputs":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
+!async function(){
+	await instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"x":763,"y":169}],"Example/Button/Simple":[{"i":4,"x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
 
+	// Time to run something :)
+	var button = instance.getNodes('Example/Button/Simple')[0].iface;
 
-// Time to run something :)
-var button = instance.getNodes('Example/Button/Simple')[0].iface;
+	log("\n>> I'm clicking the button");
+	button.clicked("'An event'");
 
-log("\n>> I'm clicking the button");
-button.clicked("'An event'");
+	var logger = instance.getNodes('Example/Display/Logger')[0].iface;
+	log("\n>> I got the output value:", logger.log);
 
-var logger = instance.getNodes('Example/Display/Logger')[0].iface;
-log("\n>> I got the output value:", logger.log);
+	log("\n>> I'm writing something to the input box");
+	var input = instance.getNodes('Example/Input/Simple')[0].iface;
+	input.data.value = 'hello wrold';
 
-log("\n>> I'm writing something to the input box");
-var input = instance.getNodes('Example/Input/Simple')[0].iface;
-input.data.value = 'hello wrold';
-
-var logger = instance.getNodes('Example/Display/Logger')[0].iface;
-log("\n>> I got the output value:", logger.log);
+	var logger = instance.getNodes('Example/Display/Logger')[0].iface;
+	log("\n>> I got the output value:", logger.log);
+}();
