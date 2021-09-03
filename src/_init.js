1f4433a32681b0c19681ece6a594e0e8d4e3ba26
StefansArya <stefansarya1@gmail.com>
2021-09-03 23:32:26 +0700

    Add feature to load modules and it's related HTML/style interface from URL

--- src/_init.js
+++ src/_init.js
@@ -22,16 +22,49 @@ Blackprint.Addons = function(name){
 	return this.Addons[name];
 };
 
-Blackprint.loadModuleFromURL = async function(url){
+Blackprint.loadModuleFromURL = async function(url, options){
 	if(url.constructor !== Array) url = [url];
 
 	if(!isBrowser)
 		return await Promise.all(url.map(v=> import(v)));
 
+	let _loadBrowserInterface = Blackprint.loadBrowserInterface;
+	if(options != null && options.loadBrowserInterface === false)
+		Blackprint.loadBrowserInterface = false;
+
 	for (var i = 0; i < url.length; i++) {
 		Blackprint._loadingURL = url[i].replace(/\?.*?$/m, '');
+
+		// Let's expect it's nodes.sf.mjs related with nodes.min.mjs
+		if(/\.sf\.mjs$/m.test(Blackprint._loadingURL)){
+			let temp = Blackprint._loadingURL.replace(/\.sf\.mjs$/m, '.min.mjs');
+
+			if(!(temp in Blackprint.modulesURL))
+				throw `"${temp}" should be loaded before "${Blackprint.modulesURL}"`;
+
+			Blackprint._loadingURL = temp;
+		}
+
 		await import(url[i]);
 	}
 
+	Blackprint.loadBrowserInterface = _loadBrowserInterface;
 	delete Blackprint._loadingURL;
+};
+
+Blackprint.loadBrowserInterface = true;
+Blackprint.hasBrowserInterface = async function(options){
+	if(!isBrowser || !Blackprint.loadBrowserInterface || !Blackprint._loadingURL)
+		return;
+
+	let noStyle = Blackprint.loadBrowserInterface !== 'without-css';
+	if(options != null && options.css === false)
+		noStyle = false;
+
+	let url = Blackprint._loadingURL.replace(/(|\.min|\.es6)\.(js|mjs|ts)$/m, '');
+
+	if(!noStyle)
+		sf.loader.css([url+'.sf.css']);
+
+	await sf.loader.mjs([url+'.sf.js']);
 }
