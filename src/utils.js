90678d947501c086a42e988e04c53829a924e820
StefansArya <stefansarya1@gmail.com>
2021-09-04 20:09:31 +0700

    Small performance improvement

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,12 @@
 function deepProperty(obj, path, value){
+	var temp;
 	if(value !== void 0){
 		for(var i = 0, n = path.length-1; i < n; i++){
-			if(obj[path[i]] === void 0)
-				obj[path[i]] = {};
+			temp = path[i];
+			if(obj[temp] === void 0)
+				obj[temp] = {};
 
-			obj = obj[path[i]];
+			obj = obj[temp];
 		}
 
 		obj[path[i]] = value;
@@ -12,9 +14,7 @@ function deepProperty(obj, path, value){
 	}
 
 	for(var i = 0; i < path.length; i++){
-		obj = obj[path[i]];
-
-		if(obj === void 0)
+		if((obj = obj[path[i]]) === void 0)
 			return;
 	}
 
