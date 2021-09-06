446605097f9799806b6b6fa433cb1106f249d5e7
StefansArya <stefansarya1@gmail.com>
2021-09-06 11:18:49 +0700

    Add callback when diving thru the object

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,12 @@
-function deepProperty(obj, path, value){
+function deepProperty(obj, path, value, onCreate){
 	var temp;
 	if(value !== void 0){
 		for(var i = 0, n = path.length-1; i < n; i++){
 			temp = path[i];
-			if(obj[temp] === void 0)
+			if(obj[temp] === void 0){
 				obj[temp] = {};
+				onCreate && onCreate(obj[temp]);
+			}
 
 			obj = obj[temp];
 		}
