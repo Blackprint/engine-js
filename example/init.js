8b336739331b2ac8c6756c114b9fee07232b0e24
StefansArya <stefansarya1@gmail.com>
2021-09-27 23:33:48 +0700

    Update example by accessing iface with the ID instead

--- example/init.js
+++ example/init.js
@@ -237,21 +237,22 @@ var Blackprint = require('../dist/engine.min.js');
 !async function(){
 	let instance = new Blackprint.Engine();
 
-	await instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"x":763,"y":169}],"Example/Button/Simple":[{"i":4,"x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
+	await instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"id":"myLogger","x":763,"y":169}],"Example/Button/Simple":[{"i":4,"id":"myButton","x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"id":"myInput","x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
 
 	// Time to run something :)
-	var button = instance.getNodes('Example/Button/Simple')[0].iface;
+	var button = instance.iface.myButton;
 
 	log("\n>> I'm clicking the button");
 	button.clicked("'An event'");
 
-	var logger = instance.getNodes('Example/Display/Logger')[0].iface;
+	var logger = instance.iface.myLogger;
 	log("\n>> I got the output value:", logger.log);
 
 	log("\n>> I'm writing something to the input box");
-	var input = instance.getNodes('Example/Input/Simple')[0].iface;
+	var input = instance.iface.myInput;
 	input.data.value = 'hello wrold';
 
-	var logger = instance.getNodes('Example/Display/Logger')[0].iface;
+	// you can also use getNodes if you haven't set the ID
+	logger = instance.getNodes('Example/Display/Logger')[0].iface;
 	log("\n>> I got the output value:", logger.log);
 }();
