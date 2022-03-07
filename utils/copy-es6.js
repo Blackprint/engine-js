73de0bce7518ac1d72390a6f68c8ba4a7d4d950d
StefansArya <stefansarya1@gmail.com>
2022-03-07 17:44:15 +0700

    Copy for es6 module before publish

---
+++ utils/copy-es6.js
@@ -0,0 +1,57 @@
+let fs = require('fs');
+
+let compiled = fs.readFileSync('./dist/engine.min.js', 'utf8');
+if(!compiled.includes(' MIT Licensed */'))
+	throw new Error("Can't find template when copying the compiled module for 'engine.es6.js'");
+
+compiled = compiled.replace(' MIT Licensed */', ' MIT Licensed */ let module={exports:{}};');
+
+if(!compiled.includes('//# sourceMappingURL=engine.min.js.map'))
+	throw new Error("Can't find template when copying the compiled module for 'engine.es6.js'");
+
+compiled = compiled.replace('//# sourceMappingURL=engine.min.js.map', `
+const {
+	settings,
+	createContext,
+	getContext,
+	loadScope,
+	allowModuleOrigin,
+	Sketch,
+	Port,
+	loadModuleFromURL,
+	nodes,
+	Node,
+	registerNode,
+	registerInterface,
+	Interface,
+	Engine,
+	Environment,
+	utils,
+	OutputPort,
+	InputPort,
+} = module.exports;
+
+export default window.Blackprint;
+export {
+	settings,
+	createContext,
+	getContext,
+	loadScope,
+	allowModuleOrigin,
+	Sketch,
+	Port,
+	loadModuleFromURL,
+	nodes,
+	Node,
+	registerNode,
+	registerInterface,
+	Interface,
+	Engine,
+	Environment,
+	OutputPort,
+	InputPort,
+};
+
+//# sourceMappingURL=engine.min.js.map`);
+
+fs.writeFileSync('./dist/engine.es6.js', compiled);
