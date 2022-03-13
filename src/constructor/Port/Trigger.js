ca45a34d374518ef2edb135c20a0c0178a85a1e3
StefansArya <stefansarya1@gmail.com>
2022-03-13 15:29:49 +0700

    Disallow use of arrow function

--- src/constructor/Port/Trigger.js
+++ src/constructor/Port/Trigger.js
@@ -1,6 +1,11 @@
 // This port will be used as a trigger or callable input port
 // func = callback when the port was being called as a function
 BP_Port.Trigger = function(func){
+	// prohibit "() => {}" or "async ()=>{}"
+	let i = func.toString().indexOf('=>');
+	if(i !== -1 && i < 15)
+		throw new Error("Please use 'function(){}' instead of arrow function'()=>{}' when using 'Blackprint.Port.Trigger' type");
+
 	return {
 		portFeature: BP_Port.Trigger,
 		default: func
