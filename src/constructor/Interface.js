978478c61ee7250ff881394c4f352a3ee7263640
StefansArya <stefansarya1@gmail.com>
2022-05-14 16:01:19 +0700

    Add node for handling environment variables

--- src/constructor/Interface.js
+++ src/constructor/Interface.js
@@ -83,6 +83,8 @@ Blackprint.Interface = class Interface extends Blackprint.Engine.CustomEvent{
 		// this.title = 'No Title';
 		this.importing = true;
 		this.node = node;
+
+		// ToDo: remove this
 		this.env = Blackprint.Environment.map;
 	}
 
---
+++ src/nodes/Env.js
@@ -0,0 +1,80 @@
+// Environment variable data type will always be a string
+Blackprint.nodes.BP.Env = {
+	Get: class extends Blackprint.Node {
+		static output = {Val: String};
+		constructor(instance){
+			super(instance);
+
+			let iface = this.setInterface('BPIC/BP/Env/Get');
+			iface.data = {};
+			iface.title = 'EnvGet';
+			iface.type = 'bp-env-get';
+		}
+	},
+	Set: class extends Blackprint.Node {
+		static input = {Val: String};
+		constructor(instance){
+			super(instance);
+
+			let iface = this.setInterface('BPIC/BP/Env/Set');
+			iface.data = {};
+			iface.title = 'EnvSet';
+			iface.type = 'bp-env-set';
+		}
+		update(){
+			Blackprint.Environment.set(this.iface.data.name, this.input.Val);
+		}
+	},
+};
+
+// ==== Interface ====
+// Register when ready
+function BPEnvInit(){
+	class BPEnvGetSet extends Blackprint.Interface {
+		imported(data){
+			if(!data.name) throw new Error("Parameter 'name' is required");
+			this.data.name = data.name;
+
+			// Listen for name change, only if Blackprint.Sketch was exist
+			if(Blackprint.Sketch != null){
+				this._nameListener = ({ old, now }) => {
+					if(this.data.name !== old) return;
+					this.data.name = now;
+				};
+
+				Blackprint.on('environment-renamed', this._nameListener);
+			}
+		}
+		destroy(){
+			if(this._nameListener == null) return;
+			Blackprint.off('environment-renamed', this._nameListener);
+		}
+	};
+
+	Blackprint.registerInterface('BPIC/BP/Env/Get',
+	class extends BPEnvGetSet {
+		imported(data){
+			super.imported(data);
+			this._listener = v => {
+				if(v.key !== this.data.name) return;
+				this.ref.Output.Val = v.value;
+			};
+
+			Blackprint.on('environment-changed environment-added', this._listener);
+			this.ref.Output.Val = Blackprint.Environment.map[this.data.name];
+		}
+		destroy(){
+			super.destroy();
+
+			if(this._listener == null) return;
+			Blackprint.off('environment-changed environment-added', this._listener);
+		}
+	});
+
+	Blackprint.registerInterface('BPIC/BP/Env/Set',
+	class extends BPEnvGetSet { });
+}
+
+if(globalThis.sf && globalThis.sf.$)
+	globalThis.sf.$(BPEnvInit);
+else BPEnvInit();
