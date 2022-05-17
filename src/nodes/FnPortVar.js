bf04bfca2166e466ae7c519f45fa8fe23bc3d7d6
StefansArya <stefansarya1@gmail.com>
2022-05-18 01:54:33 +0700

    Finishing input and output port as node

--- src/nodes/FnPortVar.js
+++ src/nodes/FnPortVar.js
@@ -11,6 +11,7 @@ Blackprint.nodes.BP.FnVar = {
 			iface.title = 'FnInput';
 
 			iface.enum = _InternalNodeEnum.BPFnVarInput;
+			iface._dynamicPort = true; // Port is initialized dynamically
 		}
 	},
 	Output: class extends Blackprint.Node {
@@ -25,10 +26,11 @@ Blackprint.nodes.BP.FnVar = {
 			iface.title = 'FnOutput';
 
 			iface.enum = _InternalNodeEnum.BPFnVarOutput;
+			iface._dynamicPort = true; // Port is initialized dynamically
 		}
 		update(){
 			let id = this.iface.data.name;
-			this.node.refOutput[id] = this.ref.Input[id];
+			this.refOutput[id] = this.ref.Input.Val;
 		}
 	},
 };
@@ -52,8 +54,39 @@ function BPFnVarInit(){
 		}
 		imported(data){
 			super.imported(data);
+			let ports = this._parentFunc.ref.IInput;
+			let node = this.node;
+
+			// Create temporary port if the main function doesn't have the port
+			let name = data.name;
+			if(!(name in ports)){
+				let iPort = node.createPort('output', name, null); // null = any type
+				iPort.onConnect = (cable, port) => {
+					delete iPort.onConnect;
+
+					cable.disconnect();
+					node.deletePort('output', 'Val');
+
+					let portType = port.feature != null ? port.feature(port.type) : port.type;
+					let newPort = node.createPort('output', 'Val', portType);
+					newPort.connectCable(cable);
+
+					this._parentFunc.addPort(port, name);
+					this._addListener();
+				};
+			}
+			else{
+				let port = ports[name];
+				let portType = port.feature != null ? port.feature(port.type) : port.type;
+				node.createPort('output', 'Val', portType);
+				this._addListener();
+			}
+		}
+		_addListener(){
+			let data = this.data;
+
 			this._listener = v => {
-				if(v.key !== this.data.name) return;
+				if(v.key !== data.name) return;
 				this.ref.Output.Val = v.value;
 			};
 
@@ -75,7 +108,33 @@ function BPFnVarInit(){
 		}
 		imported(data){
 			super.imported(data);
-			this.node.refOutput = this._parentFunc.ref.Output;
+			let ports = this._parentFunc.ref.IOutput;
+			let node = this.node;
+
+			node.refOutput = this._parentFunc.ref.Output;
+
+			// Create temporary port if the main function doesn't have the port
+			let name = data.name;
+			if(!(name in ports)){
+				let iPort = node.createPort('input', name, null); // null = any type
+				iPort.onConnect = (cable, port) => {
+					delete iPort.onConnect;
+
+					cable.disconnect();
+					node.deletePort('input', 'Val');
+
+					let portType = port.feature != null ? port.feature(port.type) : port.type;
+					let newPort = node.createPort('input', 'Val', portType);
+					newPort.connectCable(cable);
+
+					this._parentFunc.addPort(port, name);
+				};
+			}
+			else {
+				let port = ports[name];
+				let portType = port.feature != null ? port.feature(port.type) : port.type;
+				node.createPort('input', 'Val', portType);
+			}
 		}
 	});
 }
