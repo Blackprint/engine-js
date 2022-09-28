0f3c47c44466589143dc30058e061f91591cde1f
StefansArya <stefansarya1@gmail.com>
2022-09-28 14:09:15 +0700

    Update example for Node.js

--- example/init.js
+++ example/init.js
@@ -6,6 +6,7 @@
 // import Blackprint from 'https://cdn.skypack.dev/@blackprint/engine@0.6';
 
 // For Node
+require('source-map-support').install();
 var Blackprint = require('./../../dist/engine.min.js');
 // var Blackprint = require('@blackprint/engine');
 
@@ -39,6 +40,7 @@ var Blackprint = require('./../../dist/engine.min.js');
 				set value(val){
 					theValue = val;
 					iface.changed(val);
+					iface.node.routes.routeOut();
 				},
 			};
 		}
@@ -56,7 +58,7 @@ var Blackprint = require('./../../dist/engine.min.js');
 
 			// node.data.value === text;
 			node.output.Value = this.data.value;
-			// node.syncOut('data', {value: this.data.value});
+			node.syncOut('data', {value: this.data.value});
 
 			// This will call every connected node
 			node.output.Changed();
@@ -69,6 +71,7 @@ var Blackprint = require('./../../dist/engine.min.js');
 		set log(val){
 			this._log = val;
 			console.log("Example/Logger log:", val);
+			this.node.syncOut('log', val);
 		}
 	});
 
@@ -149,13 +152,49 @@ var Blackprint = require('./../../dist/engine.min.js');
 		request(cable){
 			// Only run once this node never been executed
 			// Return false if no value was changed
-			if(this.executed === true)
-				return false;
+			if(this.executed === true) return false;
 
 			console.warn('Math/Random: Value request for port:', cable.output.name, "from node:", cable.input.iface.title);
 
 			// Let's create the value for him
 			this.input['Re-seed']();
+		}
+	});
+
+	Blackprint.registerNode('Example/Dummy/UpdateTest', class extends Blackprint.Node {
+		static input = {
+			A1: String,
+			A2: String,
+		};
+	
+		static output = {
+			B1: String,
+			B2: String,
+		};
+	
+		constructor(instance){
+			super(instance);
+	
+			let iface = this.setInterface(); // Let's use default node interface
+			iface.title = "Pass data only";
+	
+			// iface.on('port.value', ({ port, target }) => {
+			// 	console.log(port, target);
+			// 	if(port.source !== 'input') return;
+			// 	this[port.name] = target.value;
+			// });
+		}
+	
+		update(){
+			let index = this.iface.id || this.instance.ifaceList.indexOf(this.iface);
+			// console.error("UpdateTest "+index+"> Updating ports");
+	
+			// if(this.input.A1 !== this.A1) console.error("A1 from event listener value was mismatched");
+			// if(this.input.A2 !== this.A2) console.error("A2 from event listener value was mismatched");
+	
+			this.output.B1 = this.input.A1;
+			this.output.B2 = this.input.A2;
+			// console.log("UpdateTest "+index+"> Updated");
 		}
 	});
 
@@ -172,27 +211,45 @@ var Blackprint = require('./../../dist/engine.min.js');
 			iface.description = 'Print anything into text';
 		}
 
-		init(){
-			let node = this;
-			let iface = this.iface;
+		_refreshLogger(val){
+			if(this.ref.IInput.Any.cables.length > 1)
+				this.iface.log = JSON.stringify(val);
+			else {
+				let val = this.ref.Input.Any?.[0];
 
-			function refreshLogger(val){
-				iface.log = JSON.stringify(val);
+				if(val === null)
+					this.iface.log = "null";
+				else if(val === undefined)
+					this.iface.log = "undefined";
+				else if(typeof val === 'object')
+					this.iface.log = JSON.stringify(val);
+				else this.iface.log = val;
 			}
+		}
 
+		init(){
 			// Let's show data after new cable was connected or disconnected
-			iface.on('cable.connect cable.disconnect', function(){
+			this.iface.on('cable.connect cable.disconnect', function(){
 				console.log("Display/Logger: A cable was changed on Logger, now refresing the input element");
-				refreshLogger(node.input.Any);
+				this.update();
 			});
 
-			iface.input.Any.on('value', function({ target }){
+			this.iface.input.Any.on('value', function({ target }){
 				console.log("Display/Logger: I connected to", target.name, "target from", target.iface.title, "that have new value:", target.value);
-
-				// Let's take all data from all connected nodes
-				// Instead showing new single data-> val
-				refreshLogger(node.input.Any);
 			});
+		}
+
+		update(){
+			let { Input } = this.ref;
+
+			// Let's take all data from all connected nodes
+			// Instead showing new single data-> val
+			this._refreshLogger(Input.Any);
+		}
+
+		// Remote sync in
+		syncIn(id, data){
+			if(id === 'log') this.iface.log = data;
 		}
 	});
 
@@ -218,8 +275,6 @@ var Blackprint = require('./../../dist/engine.min.js');
 
 			let iface = this.setInterface('BPIC/Example/Input');
 			iface.title = "Input";
-
-			iface.data = { value: '...' };
 		}
 
 		// Bring value from imported node to handle output
@@ -229,8 +284,19 @@ var Blackprint = require('./../../dist/engine.min.js');
 			console.warn("Input/Simple: Old data:", JSON.stringify(iface.data));
 			console.warn("Input/Simple: Imported data:", JSON.stringify(data));
 
-			iface.data = data;
+			if(data === undefined) return;
+	
+			// Use object assign to avoid replacing the object reference
+			Object.assign(iface.data, data);
 			this.output.Value = data.value;
+		}
+
+		// Remote sync in
+		syncIn(id, data){
+			if(id === 'data'){
+				Object.assign(this.iface.data, data);
+				this.changed();
+			}
 		}
 	});
 
@@ -239,8 +305,8 @@ var Blackprint = require('./../../dist/engine.min.js');
 !async function(){
 	let instance = new Blackprint.Engine();
 
-	// await instance.importJSON('{"_":{"moduleJS":[],"functions":{"Test":{"id":"Test","title":"Test","description":"No description","vars":["shared"],"privateVars":["private"],"structure":{"BP/Fn/Input":[{"i":0,"x":389,"y":100,"z":3,"output":{"A":[{"i":2,"name":"A"}],"Exec":[{"i":2,"name":"Exec"}]}}],"BP/Fn/Output":[{"i":1,"x":973,"y":228,"z":14}],"Example/Math/Multiply":[{"i":2,"x":656,"y":99,"z":8,"output":{"Result":[{"i":3,"name":"Val"},{"i":9,"name":"Val"}]}},{"i":10,"x":661,"y":289,"z":4,"output":{"Result":[{"i":5,"name":"Val"},{"i":1,"name":"Result1"}]}}],"BP/Var/Set":[{"i":3,"x":958,"y":142,"z":9,"data":{"name":"shared","scope":2}},{"i":5,"x":971,"y":333,"z":2,"data":{"name":"private","scope":1},"route":{"i":1}}],"BP/Var/Get":[{"i":4,"x":387,"y":461,"z":5,"data":{"name":"shared","scope":2},"output":{"Val":[{"i":8,"name":"Any"}]}},{"i":6,"x":389,"y":524,"z":0,"data":{"name":"private","scope":1},"output":{"Val":[{"i":8,"name":"Any"}]}}],"BP/FnVar/Input":[{"i":7,"x":387,"y":218,"z":7,"data":{"name":"B"},"output":{"Val":[{"i":2,"name":"B"}]}},{"i":11,"x":386,"y":301,"z":6,"data":{"name":"Exec"},"output":{"Val":[{"i":10,"name":"Exec"}]}},{"i":12,"x":386,"y":370,"z":10,"data":{"name":"A"},"output":{"Val":[{"i":10,"name":"A"},{"i":10,"name":"B"}]}}],"Example/Display/Logger":[{"i":8,"x":661,"y":474,"z":11}],"BP/FnVar/Output":[{"i":9,"x":956,"y":69,"z":1,"data":{"name":"Result"}},{"i":14,"x":969,"y":629,"z":13,"data":{"name":"Clicked"}}],"Example/Button/Simple":[{"i":13,"x":634,"y":616,"z":12,"output":{"Clicked":[{"i":14,"name":"Val"}]}}]}}}},"Example/Math/Random":[{"i":0,"x":512,"y":76,"z":0,"output":{"Out":[{"i":5,"name":"A"}]},"route":{"i":5}},{"i":1,"x":512,"y":242,"z":1,"output":{"Out":[{"i":5,"name":"B"}]}}],"Example/Display/Logger":[{"i":2,"x":986,"y":282,"z":2,"id":"myLogger"}],"Example/Button/Simple":[{"i":3,"x":244,"y":64,"z":6,"id":"myButton","output":{"Clicked":[{"i":5,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":4,"x":238,"y":279,"z":4,"id":"myInput","data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":2,"name":"Any"}]}}],"BPI/F/Test":[{"i":5,"x":738,"y":138,"z":5,"output":{"Result1":[{"i":2,"name":"Any"}],"Result":[{"i":2,"name":"Any"}],"Clicked":[{"i":6,"name":"Exec"}]},"route":{"i":6}}],"Example/Math/Multiply":[{"i":6,"x":1032,"y":143,"z":3}]}');
-	await instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"id":"myLogger","x":763,"y":169}],"Example/Button/Simple":[{"i":4,"id":"myButton","x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"id":"myInput","x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
+	await instance.importJSON('{"_":{"moduleJS":[],"functions":{"Test":{"id":"Test","title":"Test","description":"No description","vars":["shared"],"privateVars":["private"],"structure":{"BP/Fn/Input":[{"i":0,"x":389,"y":100,"z":3,"output":{"A":[{"i":2,"name":"A"}],"Exec":[{"i":2,"name":"Exec"}]}}],"BP/Fn/Output":[{"i":1,"x":973,"y":228,"z":14}],"Example/Math/Multiply":[{"i":2,"x":656,"y":99,"z":8,"output":{"Result":[{"i":3,"name":"Val"},{"i":9,"name":"Val"}]}},{"i":10,"x":661,"y":289,"z":4,"output":{"Result":[{"i":5,"name":"Val"},{"i":1,"name":"Result1"}]}}],"BP/Var/Set":[{"i":3,"x":958,"y":142,"z":9,"data":{"name":"shared","scope":2}},{"i":5,"x":971,"y":333,"z":2,"data":{"name":"private","scope":1},"route":{"i":1}}],"BP/Var/Get":[{"i":4,"x":387,"y":461,"z":5,"data":{"name":"shared","scope":2},"output":{"Val":[{"i":8,"name":"Any"}]}},{"i":6,"x":389,"y":524,"z":0,"data":{"name":"private","scope":1},"output":{"Val":[{"i":8,"name":"Any"}]}}],"BP/FnVar/Input":[{"i":7,"x":387,"y":218,"z":7,"data":{"name":"B"},"output":{"Val":[{"i":2,"name":"B"}]}},{"i":11,"x":386,"y":301,"z":6,"data":{"name":"Exec"},"output":{"Val":[{"i":10,"name":"Exec"}]}},{"i":12,"x":386,"y":370,"z":10,"data":{"name":"A"},"output":{"Val":[{"i":10,"name":"A"},{"i":10,"name":"B"}]}}],"Example/Display/Logger":[{"i":8,"x":661,"y":474,"z":11}],"BP/FnVar/Output":[{"i":9,"x":956,"y":69,"z":1,"data":{"name":"Result"}},{"i":14,"x":969,"y":629,"z":13,"data":{"name":"Clicked"}}],"Example/Button/Simple":[{"i":13,"x":634,"y":616,"z":12,"output":{"Clicked":[{"i":14,"name":"Val"}]}}]}}}},"Example/Math/Random":[{"i":0,"x":512,"y":76,"z":0,"output":{"Out":[{"i":5,"name":"A"}]},"route":{"i":5}},{"i":1,"x":512,"y":242,"z":1,"output":{"Out":[{"i":5,"name":"B"}]}}],"Example/Display/Logger":[{"i":2,"x":986,"y":282,"z":2,"id":"myLogger"}],"Example/Button/Simple":[{"i":3,"x":244,"y":64,"z":6,"id":"myButton","output":{"Clicked":[{"i":5,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":4,"x":238,"y":279,"z":4,"id":"myInput","data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":2,"name":"Any"}]}}],"BPI/F/Test":[{"i":5,"x":738,"y":138,"z":5,"output":{"Result1":[{"i":2,"name":"Any"}],"Result":[{"i":2,"name":"Any"}],"Clicked":[{"i":6,"name":"Exec"}]},"route":{"i":6}}],"Example/Math/Multiply":[{"i":6,"x":1032,"y":143,"z":3}]}');
+	// await instance.importJSON('{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"id":"myLogger","x":763,"y":169}],"Example/Button/Simple":[{"i":4,"id":"myButton","x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"id":"myInput","x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}');
 
 	// Time to run something :)
 	var button = instance.iface.myButton;
