97b7e44caac00f3ba08e27984c7d86b5fb0075b3
StefansArya <stefansarya1@gmail.com>
2022-11-20 15:19:46 +0700

    Improve TypeScript definition file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for Blackprint Engine, still not perfect yet
-// Project: https://github.com/Blackprint/Blackprint
+// Type definitions for Blackprint Engine
+// Project: https://github.com/Blackprint/engine-js
 // Definitions by: StefansArya <https://github.com/stefansarya>
 
 export namespace Types {
@@ -460,7 +460,7 @@ export class Node {
 	/**
 	 * Set this to true if you want .update function being called for every data changes from different input port
 	 */
-	partialUpdate: boolean = false;
+	partialUpdate: Boolean;
 	
 	/**
 	 * You mustn't use this class to manually construct Blackprint Node
@@ -708,4 +708,101 @@ export class RemoteEngine extends RemoteBase {
 	 * @param data desc
 	 */
 	onSyncIn(data: any): Promise<any>;
+}
+
+/**
+ * This can be used to import Blackprint JSON without actually loading any nodes modules or running nodes
+ * Every nodes is generated at runtime, nodes will doing nothing even get connected
+ * Skeleton node interface will be used by default
+ * 
+ * @note Can be used after you have imported skeleton module
+ */
+export class Skeleton {
+	/** Instance functions */
+	functions: {[key: string]: {
+		id: string,
+		title: string,
+		description: string,
+		vars: Array<any>,
+		privateVars: Array<any>,
+		structure: object
+	}};
+
+	/** Instance variables */
+	variables: {[key: string]: {
+		id: string,
+		title: string,
+	}};
+
+	/** You can use this for obtaining Node Interface reference by id */
+	iface: {[key: string]: SkeletonInterface};
+
+	/** You can use this for obtaining Node Interface reference by index */
+	ifaceList: Array<SkeletonInterface>;
+
+	constructor(json: string | object);
+}
+
+declare class SkeletonPort {
+	/** Node Interface who own this port */
+	iface: SkeletonInterface;
+	/** Port name */
+	name: string;
+	source: 'input' | 'output';
+	/**
+	 * Cable list that connected to this port
+	 * 
+	 * If you want to get "output <-> input" cable only
+	 * Make sure to check if this was not a cable branch with .hasBranch
+	 */
+	cables: Array<SkeletonCable>;
+}
+declare class SkeletonInterface {
+	/** Node namespace */
+	namespace: string;
+	/** Input ports */
+	input: {[key: string]: SkeletonPort};
+	/** Output ports */
+	output: {[key: string]: SkeletonPort};
+	/** Ports References */
+	ref: {
+		IInput: {[key: string]: SkeletonPort},
+		IOutput: {[key: string]: SkeletonPort}
+	};
+	x: number;
+	y: number;
+	z: number;
+	/** Node Interface's id */
+	id: string;
+	/** Node Interface's index */
+	i: number;
+	comment: string;
+	/** Node Interface's saved data */
+	data: object;
+	/** Node reference */
+	node: {
+		instance: Skeleton;
+		routes: SkeletonRoutePort;
+	};
+}
+declare class SkeletonRoutePort {
+	/** Node Interface who own this port */
+	iface: SkeletonInterface;
+	in: Array<SkeletonCable>;
+	out: SkeletonCable;
+}
+declare class SkeletonCable {
+	input: SkeletonPort;
+	output: SkeletonPort;
+	/** Return true if this was a cable from route port */
+	isRoute: Boolean;
+	/** Return true if this cable has branch */
+	hasBranch: Boolean;
+	overrideRot: Boolean;
+	/** Root cable (single cable that directly connected to output port) */
+	cableTrunk: SkeletonPort;
+	/** Cable branches list */
+	branch: Array<SkeletonCable>;
+	/** Cable branch's parent */
+	parentCable: SkeletonCable;
 }
